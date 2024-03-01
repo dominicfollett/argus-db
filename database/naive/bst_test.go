f1c8c5bf71c3892c04492a8a435c92de68b8a3fb
Dominic Follett-Smith <dominic.follett.smith@gmail.com>
2024-03-01 11:44:30 -0800

    Tidy up tests

--- database/naive/bst_test.go
+++ database/naive/bst_test.go
@@ -8,37 +8,8 @@ import (
 	"testing"
 )
 
-// TODO: Generated Tests -- revisit and refactor
-func TestBSTConcurrentInserts(t *testing.T) {
-	bst := NewBST()
-
-	var wg sync.WaitGroup
-	const numInserts = 100      // Number of inserts per goroutine
-	const concurrencyLevel = 10 // Number of goroutines
-
-	// Perform concurrent inserts
-	for i := 0; i < concurrencyLevel; i++ {
-		wg.Add(1)
-		go func(goroutineID int) {
-			defer wg.Done()
-			for j := 0; j < numInserts; j++ {
-				key := strconv.Itoa(goroutineID*numInserts + j)
-				bst.Insert(key, 1, 1)
-			}
-		}(i)
-	}
-
-	wg.Wait()
-
-	// After all inserts, check the size of the BST to ensure all inserts were successful
-	expectedSize := numInserts * concurrencyLevel
-	actualSize := len(bst.GetKeys())
-	if actualSize != expectedSize {
-		t.Errorf("Expected BST size to be %d, got %d", expectedSize, actualSize)
-	}
-}
-
-func TestBSTConcurrentInsertsWithDuplicates(t *testing.T) {
+// TestBSTHeightCalulcations tests the height of the BST after concurrent inserts.
+func TestBST(t *testing.T) {
 	bst := NewBST()
 
 	var wg sync.WaitGroup
@@ -46,94 +17,6 @@ func TestBSTConcurrentInsertsWithDuplicates(t *testing.T) {
 	const concurrencyLevel = 10
 	const duplicateEvery = 5
 
-	// Perform concurrent inserts, including duplicates
-	for i := 0; i < concurrencyLevel; i++ {
-		wg.Add(1)
-		go func(goroutineID int) {
-			defer wg.Done()
-			for j := 0; j < numInserts; j++ {
-				var key string
-				if j%duplicateEvery == 0 {
-					// Generate a duplicate key intentionally -- I'm not crazy about this
-					key = strconv.Itoa(j)
-				} else {
-					// Generate a unique key
-					key = strconv.Itoa(goroutineID*numInserts + j)
-				}
-				bst.Insert(key, 1, 1)
-			}
-		}(i)
-	}
-
-	wg.Wait()
-
-	maxPossibleSize := numInserts*concurrencyLevel - ((concurrencyLevel - 1) * (numInserts / duplicateEvery))
-	actualSize := len(bst.GetKeys())
-	if actualSize != maxPossibleSize {
-		t.Errorf("Expected BST size to be equal to %d, got %d", maxPossibleSize, actualSize)
-	}
-}
-
-func (node *Node) heightTestHelper(t *testing.T) int32 {
-	if node == nil {
-		if node.getHeight() != -1 {
-			t.Errorf("Expected height of nil node to be -1, got %d", node.getHeight())
-		}
-		return -1
-	}
-
-	left_height := node.left.heightTestHelper(t)
-	right_height := node.right.heightTestHelper(t)
-
-	expected_height := 1 + max(left_height, right_height)
-
-	if absInt32(expected_height-node.getHeight()) != 0 {
-		t.Errorf(
-			"%s: Difference between expected height: %d, and actual height: %d, is greater than zero! "+
-				"Left height: %d, Right height: %d",
-			node.key,
-			expected_height,
-			node.getHeight(),
-			left_height,
-			right_height,
-		)
-	}
-
-	return expected_height
-}
-
-func (node *Node) heightTestHelperCount(t *testing.T, count int) (int32, int) {
-	if node == nil {
-		if node.getHeight() != -1 {
-			t.Errorf("Expected height of nil node to be -1, got %d", node.getHeight())
-			count += 1
-		}
-		return -1, count
-	}
-
-	left_height, c := node.left.heightTestHelperCount(t, count)
-	count = c
-	right_height, c := node.right.heightTestHelperCount(t, count)
-	count = c
-
-	expected_height := 1 + max(left_height, right_height)
-
-	if absInt32(expected_height-node.getHeight()) != 0 {
-		count += 1
-	}
-
-	return expected_height, count
-}
-
-func TestBSTHeightCalulcations(t *testing.T) {
-	bst := NewBST()
-
-	var wg sync.WaitGroup
-	const numInserts = 100
-	const concurrencyLevel = 10
-	const duplicateEvery = 5
-
-	// TODO: I think a problem with this test is that the unique keys are always increasing??
 	for i := 0; i < concurrencyLevel; i++ {
 		wg.Add(1)
 		go func(goroutineID int) {
@@ -158,17 +41,13 @@ func TestBSTHeightCalulcations(t *testing.T) {
 	}
 
 	// Check the height of each node
-	bst.root.heightTestHelper(t)
-
-	// However I'd like to understand why it appears that all differences between expected and actual is 1 and not any other number.
-	// Can we prove this? At the very least we'd then know that the heights are be being consistently under estimated and not over estimated.
-	_, count := bst.root.heightTestHelperCount(t, 0)
+	_, count := bst.root.heightTestHelper(t, 0)
 	if count != 0 {
 		t.Errorf("Total differences %d", count)
 	}
 }
 
-func TestWithRandomKeys(t *testing.T) {
+func TestBSTWithRandomKeys(t *testing.T) {
 
 	keys := []string{"T", "X", "G", "L", "E", "Q", "M", "H", "O", "I", "B", "Z", "A", "V", "S", "R", "K", "P", "C", "D", "U", "F", "N", "W", "Y", "J"}
 	rand.Shuffle(len(keys), func(i, j int) { keys[i], keys[j] = keys[j], keys[i] })
@@ -205,11 +84,37 @@ func TestWithRandomKeys(t *testing.T) {
 	}
 
 	// Check the height of each node
-	bst.root.heightTestHelper(t)
-
-	// Count the number of differences between expected and actual heights
-	_, count := bst.root.heightTestHelperCount(t, 0)
+	_, count := bst.root.heightTestHelper(t, 0)
 	if count != 0 {
 		t.Errorf("Total differences %d", count)
 	}
+}
+
+func (node *Node) heightTestHelper(t *testing.T, count int) (int32, int) {
+	if node == nil {
+		if node.getHeight() != -1 {
+			t.Errorf("Expected height of nil node to be -1, got %d", node.getHeight())
+		}
+		return -1, count
+	}
+
+	left_height, count := node.left.heightTestHelper(t, count)
+	right_height, count := node.right.heightTestHelper(t, count)
+
+	expected_height := 1 + max(left_height, right_height)
+
+	if absInt32(expected_height-node.getHeight()) != 0 {
+		t.Errorf(
+			"%s: Difference between expected height: %d, and actual height: %d, is greater than zero! "+
+				"Left height: %d, Right height: %d",
+			node.key,
+			expected_height,
+			node.getHeight(),
+			left_height,
+			right_height,
+		)
+		count += 1
+	}
+
+	return expected_height, count
 }
